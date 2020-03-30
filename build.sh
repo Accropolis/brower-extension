82a99a16e455b6a28cedcf41caf20831e7c0a532
David Jeansen <EsKuel@gmail.com>
2020-03-30 19:26:30 +0200

    FIX: Replace SVG icons with PNG

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ zip accropolis-extension.zip \
     manifest.json \
     main.js \
     lib/browser-polyfill.js \
-    icons/accropolis.svg \
+    icons/accropolis48.png \
+    icons/accropolis96.png \
     _locales/fr/messages.json
